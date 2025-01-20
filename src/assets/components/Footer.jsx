1fce0c122fd0607bff4dd3bf04d9c97d305ab64b
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2025-01-20 13:16:40 +0100

    fixing footer part2

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -9,9 +9,9 @@ function Footer() {
             <div>
                 <div>
                     <span></span>
-                    <span class="footer-text">PocketArt is a platform dedicated to making art accessible to everyone, inspiring exploration of beauty, history, and personal growth through artistic discovery. Our website features images made possible by the open access policy of the Harvard Art Museum, with all copyrights belonging to them. This initiative allows us to share and celebrate their remarkable collection with a wider audience.</span>
+                    <span className="footer-text"><span className="footer-name">PocketArt</span> is a platform dedicated to making art accessible to everyone, inspiring exploration of beauty, history, and personal growth through artistic discovery. Our website features images made possible by the open access policy of the Harvard Art Museum, with all copyrights belonging to them. This initiative allows us to share and celebrate their remarkable collection with a wider audience.</span>
                 </div>
-                <div class="footer-year">PocketArt {getYear()}</div>
+                <div className="footer-year">PocketArt {getYear()}</div>
             </div>
         </>
     )
