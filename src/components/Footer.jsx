d412cb90918c7ac4339bd02c3c18edcb9d13fd27
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2025-01-21 13:47:15 +0100

    fixing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import Dyck from '../assets/img/Dyck.jpg';
+
 function Footer() {
     let getYear = () => {
         return new Date().getFullYear();
@@ -6,9 +8,9 @@ function Footer() {
 
     return (
         <>
-            <div>
-                <div>
-                    <span><img src="" alt="an image of an artist" /></span>
+            <div className="footer">
+                <div className="footer-container">
+                    <span><img src={Dyck} alt="an image of an artist" className="about-us-img" /></span>
                     <span className="footer-text"><span className="footer-name">PocketArt</span> is a platform dedicated to making art accessible to everyone, inspiring exploration of beauty, history, and personal growth through artistic discovery. Our website features images made possible by the open access policy of the Harvard Art Museum, with all copyrights belonging to them. This initiative allows us to share and celebrate their remarkable collection with a wider audience.</span>
                 </div>
                 <div className="footer-year">PocketArt {getYear()}</div>
