658d315f5e688e991cb46bbcbd0b0596cc38b54e
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2025-01-23 13:28:19 +0100

    small fixes nav and landing site

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,20 +6,21 @@ import Footer from './Footer.jsx'
 
 function Home() {
     return (
-        <div className="home">
-            <div className="home-img-opacity"> </div>
-            <Nav />
-            <div className="home-text-container">
-                <div className="home-text-1">
-                    <span className="discover">discover </span>
-                    <span id="art">Art</span>
+        <div className="background-home">
+            <div className="home">
+                <Nav />
+                <div className="home-text-container">
+                    <div className="home-text-1">
+                        <span className="discover">discover </span>
+                        <span id="art">Art</span>
+                    </div>
+                    <div className="home-text-2">
+                        <span className="discover">  every day</span>
+                    </div>
                 </div>
-                <div className="home-text-2">
-                    <span className="discover">  every day</span>
-                </div>
+                <BigButton />
+                <Footer />
             </div>
-            <BigButton />
-            <Footer />
         </div>
 
     )
