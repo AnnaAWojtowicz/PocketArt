875098a24cdac57aadcaa97b605ea6c5d59587ce
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2025-01-23 16:04:38 +0100

    fixing positioning og big buttoins container

--- src/components/BigButton.jsx
+++ src/components/BigButton.jsx
@@ -2,12 +2,10 @@ import "./bigButton.css"
 
 function BigButton() {
     return (
-        <>
-            <div className="big-button-container">
-                <button className="big-button" id="big-button-1"><span>Today's Art</span></button>
-                <button className="big-button" id="big-button-2"><span>Quiz</span></button>
-            </div>
-        </>
+        <div className="big-buttons-container">
+            <button className="big-button" id="big-button-1"><span>Today's Art</span></button>
+            <button className="big-button" id="big-button-2"><span>Quiz</span></button>
+        </div>
     )
 }
 
