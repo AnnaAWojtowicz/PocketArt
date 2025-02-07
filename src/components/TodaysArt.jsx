6468f5da0091d6df0727e5b39ff9c9fd4efe98a7
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2025-02-07 14:28:18 +0100

    todays art grid part 1

--- src/components/TodaysArt.jsx
+++ src/components/TodaysArt.jsx
@@ -4,15 +4,51 @@ import todaysArt1 from "../assets/img/todaysart1.jpeg";
 function TodaysArt() {
     return (
         <>
-            <div className="logo-todays-art">PocketArt</div>
+            <div className="todays-art-logo">PocketArt</div>
             <div className="todays-art-container">
                 <div className="todays-art-img"><img src={todaysArt1} alt="Girl in a jacket" width="500" height="600" /></div>
+
                 <div className="todays-art-text">
-                    <div className="todays-art-artist">Artist</div>
-                    <div className="todays-art-title">Title</div>
-                    <div className="todays-art-date">Date</div>
-                    <div className="todays-art-medium">Medium</div>
-                    <div className="todays-art-culture">Culture</div>
+
+                    <div className="todays-art-info" id="todays-art-artist">
+                        <span className="todays-art-label">Artist</span>
+                        <span className="todays-art-details-container">
+                            <div className="todays-art-details-1">John La Farge</div>
+                            <div className="todays-art-details-2">American (New York, NY 1835 - 1910 Providence, RI)</div>
+                        </span>
+                    </div>
+
+                    <div className="todays-art-info" id="todays-art-title">
+                        <span className="todays-art-label">Title</span>
+                        <span className="todays-art-details-container">
+                            <div className="todays-art-details-1">When the Morning Stars Sang Together</div>
+                            <div className="todays-art-details-2">All the Sons of God Shouted for Joy</div>
+                            <div className="todays-art-details-3">Former Title: When the Morning Stars Sang Together</div>
+                        </span>
+                    </div>
+
+                    <div className="todays-art-info" id="todays-art-date">
+                        <span className="todays-art-label">Date</span>
+                        <span className="todays-art-details-container">
+                            <div className="todays-art-details-1">c. 1884-1885</div>
+                        </span>
+                    </div>
+
+                    <div className="todays-art-info" id="todays-art-medium">
+                        <span className="todays-art-label">Medium</span>
+                        <span className="todays-art-details-container">
+                            <div className="todays-art-details-1">Stained Glass</div>
+                        </span>
+                    </div>
+
+
+                    <div className="todays-art-info" id="todays-art-culture">
+                        <span className="todays-art-label">Culture</span>
+                        <span className="todays-art-details-container">
+                            <div className="todays-art-details-1">American</div>
+                        </span>
+                    </div>
+
                 </div>
             </div>
 
