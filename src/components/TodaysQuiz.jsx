67496051f4e7c11c6f5f2b8e05a097257b6eb0b3
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-02-13 13:06:40 +0100

    todaysQuiz (and possibly alternative version of TodaysArt) fixed

--- src/components/TodaysQuiz.jsx
+++ src/components/TodaysQuiz.jsx
@@ -1,6 +1,7 @@
 import "./todaysQuiz.css";
 import todaysArt1 from "../assets/img/todaysart1.jpeg";
 import TodaysArtTextComponent from "./TodaysArtTextComponent.jsx";
+import Button from "react-bootstrap/Button";
 import { todaysArtData } from "../api/data.js";
 import NavBar2 from "./NavBar2.jsx";
 
@@ -9,7 +10,13 @@ function TodaysQuiz() {
         <>
             <div className="todays-site-container">
                 <NavBar2 />
+
                 <div className="todays-art-container">
+
+                    <div className="todays-art-title-container">
+                        <h1 className="todays-art-title">Oh, hey! Do you remember this?</h1>
+                        <h4>...if you do and you're ready for today's quiz, keep scrolling!</h4>
+                    </div>
                     <div className="todays-art-img-container"><img src={todaysArt1} alt="some art" className="todays-art-img" /></div>
 
                     <div className="todays-art-text">
@@ -18,9 +25,9 @@ function TodaysQuiz() {
                         <TodaysArtTextComponent label="Date" data1={todaysArtData[0].date} />
                         <TodaysArtTextComponent label="Work Type" data1={todaysArtData[0].workType} />
                         <TodaysArtTextComponent label="Culture" data1={todaysArtData[0].culture} />
-
-
                     </div>
+
+                    <Button type="submit" id="todays-quiz-btn" size="lg">Today's Quiz</Button>
                 </div>
             </div>
         </>
