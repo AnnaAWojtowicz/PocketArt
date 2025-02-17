694b4407b5363799be41f5659e8bbb772a070e92
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-02-17 15:40:19 +0100

    Setting FailQuizModal.jsx

---
+++ src/components/FailQuizModal.jsx
@@ -0,0 +1,37 @@
+import "./quizModal.css";
+
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function FailQuizModal({ show, handleClose }) {
+
+
+    return (
+        <>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton data-bs-theme="dark">
+                    <Modal.Title>Oh snap!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    No worries! Do you want to try again?
+                </Modal.Body>
+                <Modal.Footer className="modal-two-buttons">
+                    <Button variant="primary" onClick={handleClose}>
+                        Yes, please!
+                    </Button>
+                    <Button variant="primary">No, thanks!</Button>
+
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default FailQuizModal;
